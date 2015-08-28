a89fb5a9bd434989f1fd141d3e3be728588e0f27
cenyk1230 <cenyk1230@qq.com>
2015-08-28 10:03:38 +0800

    删除了sound.qrc

--- main.cpp
+++ main.cpp
@@ -4,12 +4,15 @@
 #include "gamegenfromfile.h"
 #include <QFile>
 #include <QDebug>
+#include <ctime>
+#include <cstdlib>
 
 int main(int argc, char *argv[])
 {
+    srand(time(0));
     QApplication a(argc, argv);
     Widget w;
-    w.setGameGen(new GameGenFromFile);
+    //w.setGameGen(new GameGenFromFile);
     //w.newGame(5);
     w.show();
     
